79c5018e33439ab5b9c1ac19223a498484d94c5a
Tonmoy-Roy <roytonmoy901@gmail.com>
2025-08-18 17:05:57 +0600

    navbar setup

---
+++ src/components/Error/Error.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div>
+            <p className='text-2xl justify-center items-center'>Page not Found !</p>
+        </div>
+    );
+};
+
+export default Error;
---
+++ src/main.jsx
@@ -0,0 +1,33 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Error from './components/Error/Error.jsx';
+import Home from './components/Home/Home.jsx';
+import Root from './components/Root/Root.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement: <Error></Error>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ]
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
